db9f2bdb518fdb7da05b50a07f29617ac791d997
jan <samjan1127@gmail.com>
2023-02-06 15:05:42 +0900

    init

---
+++ src/main/java/com/sparta/hanghaeblog/HanghaeblogApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeblogApplication.class, args);
+    }
+
+}
